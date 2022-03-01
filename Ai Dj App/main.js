9b0d6afa6ff371bd1117a8cc2665b75bcc338cad
We-Are-Coders <92448003+We-Are-Coders@users.noreply.github.com>
2022-03-01 10:12:20 -0500

    Add files via upload

---
+++ Ai Dj App/main.js
@@ -0,0 +1,45 @@
+BornforThis="";
+Undertale_Sans="";
+leftWristX = 0;
+leftWristY = 0;
+rightWristX = 0;
+rightWristY = 0;
+
+function setup(){
+    canvas = createCanvas(600,530);
+    canvas.center();
+
+    video = createCapture(VIDEO);
+    video.hide();
+    
+    poseNet = ml5.poseNet(video, modelLoaded);
+    poseNet.on('pose', gotPoses);
+}
+
+function modelLoaded() {
+    console.log('PoseNet Is Initialized');
+}
+
+function preload(){
+    BornforThis = loadSound("The Score - Born For This.mp3");
+    Undertale_Sans = loadSound("Sans.mp3");
+}
+
+function draw(){
+    image(video,0,0,600,530);
+}
+
+function gotPoses(results)
+{
+    if(results.length > 0)
+    {
+        console.log(results);
+        leftWristX = results[0].pose.leftWrist.x;
+        leftWristY = results[0].pose.leftWrist.y;
+        console.log("leftWristX = " + leftWristX +" leftWristY = "+ leftWristY);
+
+        rightWristX = results[0].pose.rightWrist.x;
+        rightWristY = results[0].pose.rightWrist.y;
+        console.log("rightWristX = "+ rightWristX +" rightWristY = "+ rightWristY);
+    }
+}
